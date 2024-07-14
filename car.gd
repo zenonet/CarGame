7d7ebd5185a0340794d203bd228c3dca79322b84
zenonet <78077158+zenonet@users.noreply.github.com>
2024-07-14 16:02:29 +0200

    Set up basic vehicle controller even though it behaves really weirdly

--- car.gd
+++ car.gd
@@ -1,1 +1,11 @@
 extends VehicleBody3D
+
+const MAX_STEER = 0.8
+const ENGINE_POWER = 3000
+
+func _ready():
+	pass
+
+func _process(delta):
+	steering = move_toward(steering, Input.get_axis("right", "left")*MAX_STEER, delta*2.5)
+	engine_force = Input.get_axis("decelerate", "accelerate") * ENGINE_POWER
