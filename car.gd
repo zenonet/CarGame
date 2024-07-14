eefe9df521f1d01475789f52ff7ae5b3d805b784
zenonet <78077158+zenonet@users.noreply.github.com>
2024-07-14 18:48:08 +0200

    Tried tweaking car controls to improve driving feel and added nitro boosts

--- car.gd
+++ car.gd
@@ -1,7 +1,7 @@
 extends VehicleBody3D
 
 const MAX_STEER = 0.8
-const ENGINE_POWER = 3000
+const ENGINE_POWER = 300
 const BOOST_ENGINE_POWER = 6000
 
 func _ready():
@@ -9,5 +9,8 @@ func _ready():
 
 func _process(delta):
 	steering = move_toward(steering, Input.get_axis("right", "left")*MAX_STEER, delta*2.5)
-	engine_force = Input.get_axis("decelerate", "accelerate") * ENGINE_POWER
+	engine_force = Input.get_axis("decelerate", "accelerate") * (BOOST_ENGINE_POWER if Input.is_action_pressed("nitro") else ENGINE_POWER)
 	brake = Input.get_action_strength("brake") * 10
+	
+	if(Input.is_action_just_released("quit")):
+		get_tree().quit()
