2d058e6b1f4bb38b67379011942096b549c62a4d
zenonet <78077158+zenonet@users.noreply.github.com>
2024-07-14 17:34:14 +0200

    Kind of fixed car controller and modelled a street for test driving

--- car.gd
+++ car.gd
@@ -2,6 +2,7 @@ extends VehicleBody3D
 
 const MAX_STEER = 0.8
 const ENGINE_POWER = 3000
+const BOOST_ENGINE_POWER = 6000
 
 func _ready():
 	pass
@@ -9,3 +10,4 @@ func _ready():
 func _process(delta):
 	steering = move_toward(steering, Input.get_axis("right", "left")*MAX_STEER, delta*2.5)
 	engine_force = Input.get_axis("decelerate", "accelerate") * ENGINE_POWER
+	brake = Input.get_action_strength("brake") * 10
