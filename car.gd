8e3b6742ffaace5e351d368b726a61c53da98198
zenonet <78077158+zenonet@users.noreply.github.com>
2024-07-14 21:14:36 +0200

    Made nitro limited and prepared builds

--- car.gd
+++ car.gd
@@ -2,15 +2,22 @@ extends VehicleBody3D
 
 const MAX_STEER = 0.8
 const ENGINE_POWER = 300
-const BOOST_ENGINE_POWER = 6000
+const BOOST_ENGINE_POWER = 600
+
+var nitro:float = 10.0
 
 func _ready():
 	pass
 
 func _process(delta):
 	steering = move_toward(steering, Input.get_axis("right", "left")*MAX_STEER, delta*2.5)
-	engine_force = Input.get_axis("decelerate", "accelerate") * (BOOST_ENGINE_POWER if Input.is_action_pressed("nitro") else ENGINE_POWER)
+	engine_force = Input.get_axis("decelerate", "accelerate") * (BOOST_ENGINE_POWER if Input.is_action_pressed("nitro") && nitro > 0 else ENGINE_POWER)
 	brake = Input.get_action_strength("brake") * 10
+	
+	if(Input.is_action_pressed("nitro")):
+		nitro -= delta
+	else:
+		nitro += delta * 0.5
 	
 	if(Input.is_action_just_released("quit")):
 		get_tree().quit()
